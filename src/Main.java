d9d09fb8f39f84d5a91dc890d14924260a2f6640
Elmar465 <elmar465@gmail.com>
2024-12-05 17:58:55 -0500

    RogersApplication Simulation

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import model.Rogers;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Rogers rogers = new Rogers();
+        rogers.displayDashboard();
+    }
+}
